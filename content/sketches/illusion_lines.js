f70bd5f248dfa0c0f31b0ebf1f40d69fe5782376
Juan Felipe Callejas F <jcallejas@unal.edu.co>
2022-08-29 22:54:30 -0500

    Ilusiones lineas correccion

--- content/sketches/illusion_lines.js
+++ content/sketches/illusion_lines.js
@@ -1,25 +1,29 @@
 function setup() {
-    createCanvas(500, 500);
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  
+  
+  for(var i1=0;i1<500;){
+    line(0, i1, 50+i1, 500);
+    i1+=50;
   }
   
-  function draw() {
-    background(220);
-    
-    
-    for(var i1=0;i1<500;){
-      line(0, i1, 50+i1, 500);
-      i1+=50;
-    }
-    
-    for(var i2=0;i2<500;){
-      line(500, 50+i2, i2, 0);
-      i2+=50;
-    }
-    
-    
-    for(var i3=0;i3<500;){
-      line(0, 500-i3, 50+i3, 0);
-      i3+=50;
-    }
-
+  for(var i2=0;i2<500;){
+    line(500, 50+i2, i2, 0);
+    i2+=50;
   }
+  
+  
+  for(var i3=0;i3<500;){
+    line(0, 500-i3, 50+i3, 0);
+    i3+=50;
+  }
+  
+  for(var i4=0;i4<500;){
+    line(450-i4, 500, 500, i4);
+    i4+=50;
+  }	
+}
