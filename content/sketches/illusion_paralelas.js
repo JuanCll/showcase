fdb16f508d9a49a2f783bc3a6eba3bb7139c4076
Juan Felipe Callejas F <jcallejas@unal.edu.co>
2022-09-02 11:50:01 -0500

    Paralelas

--- content/sketches/illusion_paralelas.js
+++ content/sketches/illusion_paralelas.js
@@ -9,10 +9,12 @@ function setup() {
     }
     for (var j = 0; j < 50; j+=10) {
       for (var k = 0; k < 400; k+=50) {
-     rect(0+(k+j/5), 20+j+j, 25, 20);
-     rect(0+(k+10-j/5), 120+j+j, 25, 20);
-     rect(0+(k+j/5), 220+j+j, 25, 20);
-     rect(0+(k+10-j/5), 320+j+j, 25, 20);
-     fill("black")
+        rect(0+(k+j/5), 20+j+j, 25, 20);
+        rect(0+(k+10-j/5), 120+j+j, 25, 20);
+        rect(0+(k+j/5), 220+j+j, 25, 20);
+        rect(0+(k+10-j/5), 320+j+j, 25, 20);
+        line(0,10,400,10)
+        stroke("gray")
+      fill("black")
   }}
   }
