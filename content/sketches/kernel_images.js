845017fd7626db204b2ac7e447274dfb6748050f
Juan Felipe Callejas F <jcallejas@unal.edu.co>
2022-09-04 00:58:30 -0500

    Kernel images

--- content/sketches/kernel_images.js
+++ content/sketches/kernel_images.js
@@ -18,7 +18,7 @@ function setup() {
   sel.option('Convolution');
   sel.option('Unsharp masking');
   
-  createCanvas(690, 455);
+  createCanvas(700, 455);
   pixelDensity(1);
   newimg = createImage(img.width, img.width);
   
