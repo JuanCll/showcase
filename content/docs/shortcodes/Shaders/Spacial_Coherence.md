9640c3a1278ca36cd887284e83f3944b2fd82252
Andrés López <anlopezgu@unal.edu.co>
2022-11-23 19:42:40 -0500

    Agregar Spacial Coherence

---
+++ content/docs/shortcodes/Shaders/Spacial_Coherence.md
@@ -0,0 +1,13 @@
+# **Spacial Coherence**
+
+{{<hint info>}}
+**Exercise**
+
+Implement your own source dataset and a mechanism to select different images from it.
+{{</hint>}}
+
+
+{{< p5-iframe sketch="/showcase/sketches/SHADERS/Spacial_Coherence/SC.js" width="325" height="325" >}}
+
+
+
---
+++ content/sketches/SHADERS/Spacial_Coherence/SC.js
@@ -0,0 +1,74 @@
+'use strict';
+
+let image_src;
+let video_src;
+let mosaic;
+// ui
+let resolution;
+let video_on;
+let mode;
+
+function preload() {
+  // paintings are stored locally in the /sketches/shaders/paintings dir
+  // and named sequentially as: p1.jpg, p2.jpg, ... p30.jpg
+  // so we pick up one randomly just for fun:
+  //image_src = loadImage(`/sketches/shaders/paintings/p${int(random(1, 3))}.jpg`);
+  image_src = loadImage(`photos/doge.jpg`);
+  video_src = createVideo(['videos/mapache.webm']);
+  video_src.hide();
+  mosaic = readShader('exSC.frag',
+           { matrices: Tree.NONE, varyings: Tree.texcoords2 });
+}
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+  textureMode(NORMAL);
+  noStroke();
+  shader(mosaic);
+  resolution = createSlider(1, 100, 30, 1);
+  resolution.position(10, 35);
+  resolution.style('width', '80px');
+  resolution.input(() => mosaic.setUniform('resolution', resolution.value()));
+  mosaic.setUniform('resolution', resolution.value());
+  video_on = createCheckbox('video', false);
+  video_on.changed(() => {
+    if (video_on.checked()) {
+      mosaic.setUniform('source', video_src);
+      video_src.loop();
+    } else {
+      mosaic.setUniform('source', image_src);
+      video_src.pause();
+    }
+  });
+  mosaic.setUniform('source', image_src);
+  video_on.position(10, 55);
+  mode = createSelect();
+  mode.position(10, 75);
+  mode.option('original');
+  mode.option('pixelator');
+  mode.selected('pixelator');
+  mode.changed(() => {
+    mosaic.setUniform('original', mode.value() === 'original');
+  });
+}
+
+function draw() {
+  // which previous exercise does this code actually solve?
+  /*
+        y                  v
+        |                  |
+  (-1,1)|     (1,1)        (0,1)     (1,1)
+  *_____|_____*            *__________*   
+  |     |     |            |          |        
+  |_____|_____|__x         | texture  |        
+  |     |     |            |  space   |
+  *_____|_____*            *__________*___ u
+  (-1,-1)    (1,-1)       (0,0)    (1,0) 
+  */
+  beginShape();
+  vertex(-1, -1, 0, 0, 1);
+  vertex(1, -1, 0, 1, 1);
+  vertex(1, 1, 0, 1, 0);
+  vertex(-1, 1, 0, 0, 0);
+  endShape();
+}
